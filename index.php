c8a88db3c8148f53f46af04ceda53bc9f2212083
Skryzor <skryzor@gmail.com>
2020-10-22 13:03:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -41,24 +41,6 @@ if($_POST["Furnizori"]!=""){
 	$Furnizori="";
 }
 
-  $sql = "SELECT ID, Furnizori FROM TvLEwEK10r.Table_1";
-  $result = $conn->query($sql);
-  echo "<br><h2>Your Input:</h2>";
-  echo '<table><tr>';
-  echo '<th>'."ID ".'</th>';
-  echo '<th>'."Furnizori ".'</th>';
-  
-  echo '</tr><tbody>';
-  if ($result->num_rows > 0) {
-	while($row = $result->fetch_assoc()) {
-		echo '<tr>';
-		echo "<td>". $row["ID"].'</td>'."<td>". $row["Furnizori"].'</td>';
-		echo '</tr>';
-	}
-  } else {
-	echo "0 results";
-  }
-
 ?>
 
 <form class="delete" action="" method="post">
@@ -79,5 +61,27 @@ if (isset($_POST['name'])){
   }
 }
 
+
+
+$sql = "SELECT ID, Furnizori FROM TvLEwEK10r.Table_1";
+  $result = $conn->query($sql);
+  echo "<br><h2>Your Input:</h2>";
+  echo '<table><tr>';
+  echo '<th>'."ID ".'</th>';
+  echo '<th>'."Furnizori ".'</th>';
+  
+  echo '</tr><tbody>';
+  if ($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		echo '<tr>';
+		echo "<td>". $row["ID"].'</td>'."<td>". $row["Furnizori"].'</td>';
+		echo '</tr>';
+	}
+  } else {
+	echo "0 results";
+  }
+
+
+  
 ?>
 </form>
