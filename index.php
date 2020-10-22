9488f5420fbb61b2ddfbaa9c9c4d989ab980afba
Skryzor <skryzor@gmail.com>
2020-10-22 12:14:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-  //echo "Connected successfully";
-  
+  echo "Connected successfully";
+
 $myfrm = '
 <!doctype html>
 <html>
