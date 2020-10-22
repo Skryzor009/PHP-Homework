b56bc2813b3e809081b53ff0cc6fb1acd8610b60
Skryzor <skryzor@gmail.com>
2020-10-22 13:10:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,87 +1,108 @@
-
-<form class="form1" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-    <h2>Placeholder Lorem Ipsum</h2>
-      Furnizori: <input type="text" name="Furnizori" value="<?php echo $Furnizori;?>">
-      <input type="submit" name="submit" value="Submit">  
+<form class="form1" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+	<h2>Placeholder Lorem Ipsum</h2>
+	Furnizori: <input type="text" name="Furnizori" value="<?php echo $Furnizori; ?>">
+	<input type="submit" name="submit" value="Submit">
 </form>
 
 <?php
 
-  //Conection to database
-  $servername = "remotemysql.com";
-  $username = "TvLEwEK10r";
-  $password = "iQ5VxN0MUr";
+//Conection to database
+$servername = "remotemysql.com";
+$username = "TvLEwEK10r";
+$password = "iQ5VxN0MUr";
 
-  // Create connection
-  $conn = new mysqli($servername, $username, $password);
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
-  // Check connection
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-  echo "Connected successfully";
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
-  if (empty($_POST["Furnizori"])) {
-    $FurnizoriErr = "Name is required";
-  } else {
-    $Furnizori = $_POST["Furnizori"];
-    if (!preg_match("/^[a-zA-Z-' ]*$/",$Furnizori)) {
-      $FurnizoriErr = "Only letters and white space allowed";
-    }
-  }
+if (empty($_POST["Furnizori"])) {
+	$FurnizoriErr = "Name is required";
+} else {
+	$Furnizori = $_POST["Furnizori"];
+	if (!preg_match("/^[a-zA-Z-' ]*$/", $Furnizori)) {
+		$FurnizoriErr = "Only letters and white space allowed";
+	}
+}
 
-if($_POST["Furnizori"]!=""){
+if ($_POST["Furnizori"] != "") {
 	$sql = "INSERT INTO TvLEwEK10r.Table_1 (Furnizori)
 		VALUES ('$Furnizori')";
 	if ($conn->query($sql) === TRUE) {
-	echo "New record created successfully";
+		echo "New record created successfully";
 	} else {
-	  echo "Error: " . $sql . "<br>" . $conn->error;
+		echo "Error: " . $sql . "<br>" . $conn->error;
 	}
-	$Furnizori="";
+	$Furnizori = "";
 }
 
 ?>
 
 <form class="delete" action="" method="post">
-      id: <input type="number" name="name" value="<?php echo $numberId;?>">
-      <button type="submit" name="sub" value="">Delete</button>
+	id: <input type="number" name="name" value="<?php echo $numberId; ?>">
+	<button type="submit" name="sub" value="">Delete</button>
 
- <?php
-
-
-if (isset($_POST['name'])){
+	<?php
 
 
-  $sql = "DELETE FROM TvLEwEK10r.Table_1 WHERE ID=".$_POST['name'];
-  if ($conn->query($sql) === TRUE) {
-	echo "Record deleted successfully";
-  } else {
-	echo "Error deleting record: " . $conn->error;
-  }
-}
+	if (isset($_POST['name'])) {
 
 
-
-$sql = "SELECT ID, Furnizori FROM TvLEwEK10r.Table_1";
-  $result = $conn->query($sql);
-  echo "<br><h2>Your Input:</h2>";
-  echo '<table><tr>';
-  echo '<th>'."ID ".'</th>';
-  echo '<th>'."Furnizori ".'</th>';
-  
-  echo '</tr><tbody>';
-  if ($result->num_rows > 0) {
-	while($row = $result->fetch_assoc()) {
-		echo '<tr>';
-		echo "<td>". $row["ID"].'</td>'."<td>". $row["Furnizori"].'</td>';
-		echo '</tr>';
+		$sql = "DELETE FROM TvLEwEK10r.Table_1 WHERE ID=" . $_POST['name'];
+		if ($conn->query($sql) === TRUE) {
+			echo "Record deleted successfully";
+		} else {
+			echo "Error deleting record: " . $conn->error;
+		}
 	}
-  } else {
-	echo "0 results";
-  }
+
+	?>
+</form>
+
+<form class="delete" action="" method="post">
+	id: <input type="number" name="edit" value="<?php echo $EditNumberID; ?>">
+	Furnizori: <input type="number" name="edit" value="<?php echo $FurnizorName; ?>">
+	<button type="submit" name="sub" value="">Edit</button>
+
+	<?php
 
 
-  
-?>
+	if (isset($_POST['edit'])) {
+
+
+		$sql = "UPDATE TvLEwEK10r.Table_1 SET name = '$FurnizorName' WHERE ID =".$_POST['edit'];
+		if ($conn->query($sql) === TRUE) {
+			echo "Record edited successfully";
+		} else {
+			echo "Error editing record: " . $conn->error;
+		}
+	}
+
+
+
+	$sql = "SELECT ID, Furnizori FROM TvLEwEK10r.Table_1";
+	$result = $conn->query($sql);
+	echo "<br><h2>Your Input:</h2>";
+	echo '<table><tr>';
+	echo '<th>' . "ID " . '</th>';
+	echo '<th>' . "Furnizori " . '</th>';
+
+	echo '</tr><tbody>';
+	if ($result->num_rows > 0) {
+		while ($row = $result->fetch_assoc()) {
+			echo '<tr>';
+			echo "<td>" . $row["ID"] . '</td>' . "<td>" . $row["Furnizori"] . '</td>';
+			echo '</tr>';
+		}
+	} else {
+		echo "0 results";
+	}
+
+
+
+	?>
 </form>
